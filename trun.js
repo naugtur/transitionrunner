0efc6de421967207d9ba97ff84594d1fee11ed97
Naugtur <naugtur@gmail.com>
2013-04-28 10:47:55 +0200

    auto-adding transition if avaliable

--- trun.js
+++ trun.js
@@ -1,42 +1,67 @@
-var animate = function(element,animationName,S){
+var animate = (function(){
 	"use strict";
-	var timeskip=0,stepCounter,repeats=0,duration=100,
-	dynamics=null;
 
-	S || (S={});
-	S.steps || (S.steps=3);
-	S.duration || (S.duration=100);
-	S.delay || (S.delay=0);
-	S.repeat || (S.repeat=1);
+	var domPrefixes = 'webkit Moz O ms Khtml'.split(' '),
+	trans  = '',support=false,
+	elm = document.createElement('div');
 
-	element.setAttribute('class',element.getAttribute('class')+' '+animationName);
-
-	function next(){
-		stepCounter++;
-		element.setAttribute('data-kframe',stepCounter);
-		if(stepCounter<S.steps){
-			setTimeout(next,S.duration);
-		}else{
-			if(repeats<S.repeat){
-				setTimeout(run,S.delay);			
+	if( elm.style.transition ) { 
+		trans  = 'transition';
+	} else {
+		for( var i = 0; i < domPrefixes.length; i++ ) {
+			if( elm.style[domPrefixes[i] + 'Transition'] !== undefined ) {
+				trans = domPrefixes[i] + 'Transition';
+				break;
 			}
 		}
 	}
-	function run(overrideS){
-		if(overrideS){
-			overrideS.delay && (S.delay = overrideS.delay);
-			overrideS.duration && (S.duration = overrideS.duration);
-			overrideS.steps && (S.steps = overrideS.steps);
-			overrideS.repeat && (S.repeat = overrideS.repeat);
-		}
-		stepCounter=0;
-		repeats++;
-		next();
 
-	}
+	return function(element,animationName,S){
+
+		var timeskip=0,stepCounter,repeats=0,duration=100,
+		dynamics=null;
+
+		S || (S={});
+		S.steps || (S.steps=3);
+		S.duration || (S.duration=100);
+		S.delay || (S.delay=0);
+		S.repeat || (S.repeat=1);
+
+		element.setAttribute('class',element.getAttribute('class')+' '+animationName);
+		if(trans !== ''){
+			element.style[trans] = 'All '+S.duration+'ms linear';
+		}
+
+		function next(){
+			stepCounter++;
+			if(stepCounter<=S.steps){
+				element.setAttribute('kframe',stepCounter);
+				setTimeout(next,S.duration);
+			}else{
+				element.removeAttribute('kframe');
+				if(repeats<S.repeat){
+					setTimeout(run,S.delay);			
+				}
+			}
+		}
+		function run(overrideS){
+			if(overrideS){
+				overrideS.delay && (S.delay = overrideS.delay);
+				overrideS.duration && (S.duration = overrideS.duration);
+				overrideS.steps && (S.steps = overrideS.steps);
+				overrideS.repeat && (S.repeat = overrideS.repeat);
+			}
+			stepCounter=0;
+			repeats++;
+			next();
+
+		}
 	setTimeout(run,S.delay); //skip to the moment after all methods are called
 	return {
 		run:run		
 	}
 
 };
+
+
+})();
